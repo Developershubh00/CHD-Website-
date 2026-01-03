109665a6027ff2bcced444d66cd8263e07abf5f4
Sujal Rana <Sran012pro@gmail.com>
2026-01-03 10:18:45 +0530

    quality management image modified (#12)

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -2,7 +2,7 @@ import { ContactForm } from "@/components/ContactForm";
 
 const Contact = () => {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen bg-background">
       <div className="py-32 px-6">
         <div className="max-w-2xl mx-auto">
           <h1 className="text-5xl md:text-7xl font-light mb-16">Contact</h1>
