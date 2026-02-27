b8411f1644726577296c00e7c00c581170952089
Shubham Saxena <developershubh00@gmail.com>
2026-02-27 12:12:11 +0530

    ..

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -43,7 +43,7 @@ const Index = () => {
         <ProductCategoriesSpatial />
         {/* <ImageGallery /> */}
         <QualitySection />
-        
+        <CertificateSection />
         <ComplianceSection />
         <ContactSection />
       </div>
